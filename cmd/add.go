db7b2ea28d95ed708da6d3068b0ed555e8aa61fe
Zudra <murilo.klug@gmail.com>
2025-04-17 11:36:27 -0300

    Adds 'add' command for task creation
    Introduces a new 'add' command to enable users to add tasks to the task list. Validates input to ensure a task description is provided and delegates task creation to the task management logic.

---
+++ cmd/add.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"errors"
+	
+	"task-tracker/internal/task"
+	"github.com/spf13/cobra"
+)
+
+func NewAddCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "add",
+		Short: "Add a task to the task list",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunAddTaskCmd(args)
+		},
+	}
+	return cmd
+}
+
+func RunAddTaskCmd(args []string) error {
+	if len(args) == 0 {
+		return errors.New("Task description is required")
+	}
+
+	description := args[0]
+	return task.AddTask(description)
+}
