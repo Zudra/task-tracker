ee4730536af91b1ca6192085954b7675a786ed90
Zudra <murilo.klug@gmail.com>
2025-04-17 12:10:23 -0300

    Adds delete command for task removal
    Introduces a new CLI command to delete tasks by ID in the task-tracker application. Validates input and integrates with the task deletion logic in the internal task package.

---
+++ cmd/delete.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"errors"
+	"strconv"
+	"task-tracker/internal/task"
+
+	"github.com/spf13/cobra"
+)
+
+func NewDeleteCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "delete",
+		Short: "Deletes a task from the task-tracker",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunDeleteTaskCmd(args)
+		},
+	}
+
+	return cmd
+}
+
+func RunDeleteTaskCmd(args []string) error {
+	if len(args) != 1 {
+		return errors.New("A Task ID is required")
+	}
+
+	TaskID := args[0]
+	
+	TaskIdInt, err := strconv.ParseInt(TaskID, 10, 32)
+	if err != nil {
+		return err
+	}
+
+	return task.DeleteTask(TaskIdInt)
+}
