a02472f74cdb6ce5fbbc7647a6e2c14ad295a33d
Zudra <murilo.klug@gmail.com>
2025-04-22 10:24:48 -0300

    Adds 'list' command to display tasks by status
    Introduces a new 'list' command using Cobra CLI to display tasks based on their status (e.g., all, todo). Implements logic to handle different statuses and integrates with the task tracker for fetching task lists.
    
    Supports better task management by enabling users to filter tasks dynamically.

---
+++ cmd/list.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"task-tracker/internal/task"
+
+	"github.com/spf13/cobra"
+)
+
+func NewListCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List the tasks registered in your tracker",
+		Long: `List all the tasks based on their status: 
+
+		task-tracker list (list all)
+		task-tracker list todo (list all todo tasks)
+		`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunListCmd(args)
+		},
+	}
+
+	return cmd
+}
+
+func RunListCmd(args []string) error {
+	if len(args) > 0 {
+		status := task.TaskStatus(args[0])
+		return task.ListTasks(status)
+	}
+
+	return task.ListTasks("all")
+}
